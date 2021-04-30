efe8c3d0dd9774e6060e1730e5c1878cc93791ef
Sebastian Robledo <sebastianrobledo@MacBook-Air-de-Sebastian.local>
2021-04-30 08:45:24 -0500

    added data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' A list with data to create ToS
+#'
+#' A list of three dataset that contain:
+#' 1. Graph: this is a citation network build with the references
+#' 2. df: A tibble with the main information of our initial
+#' dataset with some more ids
+#' 3. nodes: A tibble with the complete information of each node
+#' from graph dataset (1).
+#'
+#' @format A list with two dataset
+#'
+#' @source \url{https://www.scopus.com} \url{https://www.webofknowledge.com}
+#'
+"my_tosr_load"
